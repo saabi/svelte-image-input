40980a19f8423094a461e48b86fc1d604eef2e0d
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2025-12-10 15:44:53 -0300

    Update build files

--- _app/immutable/chunks/1.02c59f19.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.b3d5e425.js";export{t as component};
--- _app/immutable/chunks/2.26cebfa3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.1f1b0520.js";export{t as component};
